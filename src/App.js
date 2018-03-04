8fc9f763f64c3c0001671d327c48716b0d37f644
Nikita Tikhomirov <nikitati@gmail.com>
2018-03-04 17:18:24 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
-import { hot } from "react-hot-loader";
-import styled from "styled-components";
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 18px;
   text-align: center;
   color: palevioletred;
-`;
+`
+
+const zzz = ''
 
 const App = () => {
   return (
     <div>
       <Title>React here 321!</Title>
     </div>
-  );
-};
-export default hot(module)(App);
+  )
+}
+export default hot(module)(App)
