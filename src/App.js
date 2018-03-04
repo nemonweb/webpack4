889cebf250048c77fa14f731dc6b7c2d985a2c8c
Nikita Tikhomirov <nikitati@gmail.com>
2018-03-04 16:51:11 +0300

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { hot } from 'react-hot-loader'
-import styled from 'styled-components';
+import { hot } from "react-hot-loader";
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 18px;
