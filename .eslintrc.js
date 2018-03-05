16e352240c997e0d508388b391b5edfc5e15038d
Nikita Tikhomirov <nikitati@gmail.com>
2018-03-05 08:02:16 +0300

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,21 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es6: true
+    es6: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
-      jsx: true
+      jsx: true,
     },
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react"],
   rules: {
-    indent: ["error", 4],
+    indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
-    semi: ["error", "never"]
-  }
-};
+    semi: ["error", "never"],
+  },
+}
